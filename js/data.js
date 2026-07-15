4b53b8d62dde1e22f512e1f65f9ac704504614d5
runwithboram <5yjk2f6cb7@privaterelay.appleid.com>
2026-07-15 18:32:49 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,57 @@
+const appData = {
 
+    user: {
+        name: "보람",
+        goal: "SUB60"
+    },
+
+    today: {
+        training: "Easy Run",
+        distance: 8,
+        pace: "6'20\""
+    },
+
+    weekly: {
+        goal: 25,
+        current: 18
+    },
+
+    records: {
+        tenK: "1:02:17",
+        half: "2:17:59",
+        vo2: 42
+    },
+
+    races: [
+
+        {
+            id:1,
+            name:"설레임런",
+            date:"2026-09-06",
+            target:"61:10"
+        },
+
+        {
+            id:2,
+            name:"춘천 10K",
+            date:"2026-10-25",
+            target:"60:30"
+        },
+
+        {
+            id:3,
+            name:"JTBC",
+            date:"2026-11-01",
+            target:"60:10"
+        },
+
+        {
+            id:4,
+            name:"MBN 서울마라톤",
+            date:"2026-11-15",
+            target:"59:59"
+        }
+
+    ]
+
+}
